f5c80714edbda3d9fad40c3b54935fa9ec89263b
Subhradeep <subhradeepbasu2002@gmail.com>
2026-02-26 17:36:40 +0530

    Added: Test Dashboard Teacher POV

---
+++ Controllers/TestController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+
+namespace PrepMaster.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create(int id) //teacherID
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Create()
+        {
+            //return Json(new
+            //{
+            //    success = result.Success == 1,
+            //    message = result.Message
+            //});
+            return Json(new { });
+        }
+    }
+}
